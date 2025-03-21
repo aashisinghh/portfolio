6d539b9730010b3fb3f0804442d407efae641ec0
aashisinghh <aashi30singh@gmail.com>
2025-03-21 02:19:31 -0700

    sped up animations

--- constants/index.js
+++ constants/index.js
@@ -12,7 +12,7 @@ export const TYPEWRITER = ({ words, typeSpeed, cursor, cursorStyle }) => {
   );
 };
 
-export const INVIEWSLIDE = (startX = -100, delay = 0.75) => ({
+export const INVIEWSLIDE = (startX = -100, delay = 0.5) => ({
     whileInView: { opacity: 1, x: 0 },
     initial: { opacity: 0, x: startX },
     transition: { duration: 2, delay: delay },
